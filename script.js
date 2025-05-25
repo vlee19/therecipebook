fd6422e1d15e31fab91a6c69da54cdeed2dd72aa
Valerie Lee <leevalerie1990@gmail.com>
2025-05-25 17:56:46 -0400

    Added eventListener for nav bar

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 const offScreenMenu = document.querySelector('.off-screen-menu');
+const menuButton = document.querySelector('[onclick="showSidebar()"');
 
 function showSidebar(){
   offScreenMenu.classList.add('active');
+  document.addEventListener('click', outsideClick);
 }
 function hideSidebar(){
   offScreenMenu.classList.remove('active');
+  document.removeEventListener('click', outsideClick);
+}
+function outsideClick(event){
+  if(!offScreenMenu.contains(event.target) && !menuButton.contains(event.target)){
+    hideSidebar();
+  }
 }
